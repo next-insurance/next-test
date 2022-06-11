8fd053c5b562e5ca49b67a63a5ca241f58615621
Idan Rozin <idan@browzwear.com>
2022-06-11 10:53:52 +0300

    movie component styling and design

--- client/src/components/Movie.jsx
+++ client/src/components/Movie.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
-// import logo from '../assets/logo-big.png';
 
 import styles from '../styles/Movie.module.scss';
 
-const Movie = () => {
+const Movie = ({ movieId, title, released, rating, image }) => {
+  const generatMovieTitle = () => title + (released ? ` (${released})` : '');
+
+  const handleReadMore = (id) => {
+    console.log('id', id);
+  };
+
   return (
     <div className={styles.container}>
-      <img src="" alt="" />
-      <label>movie title</label>
-      <label>movie rating</label>
-      <button>read more</button>
+      <img src={image} alt={title} title={title} />
+      <label className={styles.movie__title} title={generatMovieTitle()}>
+        {generatMovieTitle()}
+      </label>
+      <label className={`${styles.movie__rating} ${rating ? '' : styles.no_rating}`}>
+        <i className={styles.star} />
+        {rating ? rating : 'No Rating Available'}
+      </label>
+      <button className={styles.show_more} onClick={() => handleReadMore(movieId)}>
+        Read more
+      </button>
     </div>
   );
 };
