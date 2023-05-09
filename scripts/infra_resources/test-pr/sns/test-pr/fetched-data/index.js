d9a17b660c1b37111944d68eef1764c764c911eb
nextinsurance-backstage <101115230+nextinsurance-backstage@users.noreply.github.com>
2023-05-09 22:09:15 +0300

    [Backstage] New SQS Queue: test-pr

---
+++ scripts/infra_resources/test-pr/sns/test-pr/fetched-data/index.js
@@ -0,0 +1,3 @@
+console.log('Hello from test-pr!');
+console.log('Hello from [{"array":"value3","flag":true,"someInput":"test1"},{"array":"value2","someInput":"test2","flag":false}]!');
+ console.log(" item in the list : test1 ") console.log(" item in the list : test2 ")
