361bd070f0331da03bea7490e2aefb0f47ba8251
nextinsurance-backstage <101115230+nextinsurance-backstage@users.noreply.github.com>
2023-05-09 22:14:57 +0300

    [Backstage] New SQS Queue: test-sns2

---
+++ scripts/infra_resources/test-sns2/sns/test-sns2/fetched-data/index.js
@@ -0,0 +1,5 @@
+console.log('Hello from test-sns2!');
+console.log('Hello from [{"array":"value3","flag":true,"someInput":"test1"},{"array":"value3","flag":true,"someInput":"test2"}]!');
+console.log(" item in the list : test1 ")
+console.log(" item in the list : test2 ")
+
